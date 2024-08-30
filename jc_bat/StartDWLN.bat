2310486a32c20ce403229bc9ed0c0234be5efa06
JinxCade <76182826+JinxCade@users.noreply.github.com>
2024-08-30 17:17:34 -0500

    Update StartDWLN.bat

--- jc_bat/StartDWLN.bat
+++ jc_bat/StartDWLN.bat
@@ -1,4 +1,4 @@
 echo.
-powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/jinxcade-media/jc_bat/DownloadUpdate.bat','DownloadUpdate.bat') 
-start DownloadUpdate.bat
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/jinxcade-media/jc_bat/DownloadUpdate.bat','du.bat') 
+start du.bat
 exit
