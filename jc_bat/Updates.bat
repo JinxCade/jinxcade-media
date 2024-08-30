5661d4bea81f0294cfca334bdf75608021a7b750
JinxCade <76182826+JinxCade@users.noreply.github.com>
2024-08-30 17:10:36 -0500

    Update Updates.bat

--- jc_bat/Updates.bat
+++ jc_bat/Updates.bat
@@ -1,9 +1,15 @@
 title Updates 
 @echo off 
 color 0A 
-cls 
-echo Sorry, it seems like we don't have the bytes to talk to the internet D:, in the meantime, check JinxCade's github for updates! echo Press any key to return back to the main program. 
-pause 
-start 
-JinxCade.bat 
+:l
+cls
+set /p o=Are you sure you want to update? (Y/N)
+if %o%==Y goto y
+if %o%==N goto n
+goto l
+:n
+start JinxCade.bat
+exit
+:y
+start StartDWLN.bat
 exit
