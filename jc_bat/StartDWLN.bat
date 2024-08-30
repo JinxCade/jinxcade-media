5a8127760c8e6fd4bcf9168abb6cc8e831aba934
JinxCade <76182826+JinxCade@users.noreply.github.com>
2024-08-30 17:15:53 -0500

    Update StartDWLN.bat

--- jc_bat/StartDWLN.bat
+++ jc_bat/StartDWLN.bat
@@ -1,9 +1,4 @@
 echo.
-title Start Downloading...
-@echo off
 powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/jinxcade-media/jc_bat/DownloadUpdate.bat','DownloadUpdate.bat') 
-echo Downloaded DownloadUpdate.bat 
-echo Starting DownloadUpdate.bat
 start DownloadUpdate.bat
-echo Started DownloadUpate.bat and Killed this process
 exit
