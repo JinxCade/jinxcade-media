265e00dea77e893fc20c0b5b83a0649c554ba6ea
JinxCade <76182826+JinxCade@users.noreply.github.com>
2024-08-30 13:12:09 -0500

    Create Updates.bat

---
+++ jc_bat/Updates.bat
@@ -0,0 +1,9 @@
+title Updates 
+@echo off 
+color 0A 
+cls 
+echo Sorry, it seems like we don't have the bytes to talk to the internet D:, in the meantime, check JinxCade's github for updates! echo Press any key to return back to the main program. 
+pause 
+start 
+JinxCade.bat 
+exit
