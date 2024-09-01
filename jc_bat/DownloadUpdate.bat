a501e59cdb571b10cf092f9ae91317fa87945182
JinxCade <76182826+JinxCade@users.noreply.github.com>
2024-09-01 15:20:54 -0500

    Add files via upload

--- jc_bat/DownloadUpdate.bat
+++ jc_bat/DownloadUpdate.bat
@@ -2,7 +2,8 @@ echo.
 @echo off 
 title DOWNLOAD 
 cls 
-echo Begin 
+echo Downloader v0.1.3
+echo Begin
 powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/jinxcade-media/jc_bat/JinxCade.bat','JinxCade.bat') 
 echo Downloaded JinxCade.bat 
 powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/jinxcade-media/jc_bat/Add-On0.bat','Add-On0.bat') 
@@ -13,6 +14,18 @@ powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://
 echo Downloaded Updates.bat 
 powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/jinxcade-media/jc_bat/JinxCadeCode.bat','JinxCadeCode.bat') 
 echo Downloaded JinxCadeCode.bat 
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/jinxcade-media/jc_bat/Music.bat','Music.bat') 
+echo Downloaded Music.bat 
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/jinxcade-media/jc_bat/RefreshMusic.bat','RefreshMusic.bat') 
+echo Downloaded RefreshMusic.bat
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/jinxcade-media/jc_bat/Add-On1.bat','Add-On1.bat') 
+echo Downloaded Add-On1.bat
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/jinxcade-media/jc_bat/Video.bat','Video.bat') 
+echo Downloaded Video.bat
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/jinxcade-media/jc_bat/0open.bat','0open.bat') 
+echo Downloaded 0open.bat
+powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://jinxcade.github.io/jinxcade-media/jc_bat/RefreshVideo.bat','RefreshVideo.bat') 
+echo Downloaded RefreshVideo.bat
 echo Finished and opening file after user presses a key
 pause
 start JinxCade.bat
